4e6cbb17447681d60b170879edb623c66f6d2a8c
slava.lobikov <lobikova12@mail.ru>
2021-10-21 18:38:15 +0300

    [PER_5]: Add contacs but should update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 import cn from 'classnames';
 
 import s from './Header.module.scss';
 import {NavLink} from "../NavLink";
 
 
-
 interface HeaderProps {
     hideReportButton?: boolean,
+    usnews?: boolean,
 }
 
 export const Header: React.FC<HeaderProps> = ({
                                                   hideReportButton,
+                                                  usnews,
                                               }) => {
     return (
         <header>
             <div className={s.header}>
-                <Image
-                    src={'https://vesti.bntu.by/sites/all/themes/newspro/images/Logo-vesti-2014.png'}
-                    alt={'Вести'}
-                    height={63}
-                    width={210}
-                />
+                <Link href={'/'}>
+                    <Image
+                        src={'https://vesti.bntu.by/sites/all/themes/newspro/images/Logo-vesti-2014.png'}
+                        alt={'Вести логотип - ссылка на главную страницу'}
+                        height={63}
+                        width={210}
+                    />
+                </Link>
                 <div className={s.inputContainer}>
                     <input placeholder={'Поиск по новостям'} className={cn(s.input,
                         {[s.inputWithReportButton]: !hideReportButton})} type="text"/>
@@ -30,9 +34,13 @@ export const Header: React.FC<HeaderProps> = ({
 
                 </div>
 
-                {!hideReportButton && <button className={s.btnReport}>Сообщить новость</button>}
+                {!hideReportButton && <Link href={'/unews'}>
+                    <button className={cn(s.btnReport,
+                        {[s.hidden]: usnews})}>Сообщить новость</button>
+                </Link>}
+
             </div>
-            {!hideReportButton && <nav>
+            {!hideReportButton && !usnews && <nav>
                 <ul className={s.ul}>
                     <li className={s.li}>
                         <NavLink className={'link'} href={'/daynews'}>
@@ -44,6 +52,23 @@ export const Header: React.FC<HeaderProps> = ({
                     <li>В мире</li>
                     <li>Происшествия</li>
                     <li>Подвал</li>
+                </ul>
+                <div className={s.line}></div>
+            </nav>}
+            {usnews && <nav>
+                <ul className={s.ul}>
+                    <li className={s.li}>
+                        <NavLink className={'link'} href={'/unews'}>
+                            Связь с редакцией
+                        </NavLink>
+                    </li>
+                    <li className={s.li}>
+                        <NavLink className={'link'} href={'/contacts'}>
+                            Контакты
+                        </NavLink>
+                    </li>
+
+
                 </ul>
                 <div className={s.line}></div>
             </nav>}
